9ba44732d0015f4bc2a5df230829957207e3229c
testcasereducer <testcasereducer@gmail.com>
2023-05-15 11:40:13 -0500

    Agregando middleware

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ origins = [
     "http://127.0.0.1",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://test-case-recuder-front-end.vercel.app/",
+    ""
 ]
 
 app.add_middleware(
